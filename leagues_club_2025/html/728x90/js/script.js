f991114d8f151194ac97a519428567b4e514f392
Ganesh Gujjeti (Annalect) <ganesh.gujjeti@annalect.com>
2025-06-17 19:21:54 +0530

    Initial commit

---
+++ leagues_club_2025/html/728x90/js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let tl = gsap.timeline({ repeat: false, repeatDelay: 0 });
+
+    tl.from(".text1, .text2", { 
+        y: 30, 
+        duration: 1.2, 
+        ease: "power4.out", 
+        opacity:0,
+        stagger: 0.1
+    }).from(".booknowCTA", { 
+        y: 30, 
+        duration: 1.0,  
+        opacity:0,
+        ease: "power4.out"
+    }, "-=1") 
+.from(".logos_wrapper span, .tagline", { 
+        y: 30, 
+        duration: 1.0,  
+        opacity:0,
+        ease: "power4.out",
+        stagger: 0.1
+    }, "-=1") 
+     
+});
